87116cbeb3a397f4fe1aef76eccad36203a120ee
Wen Chen <wen@wormbase.org>
2017-11-24 15:17:26 -0800

    Eliminate Positive but not curatible

--- matchPaperID_MA.pl
+++ matchPaperID_MA.pl
@@ -5,6 +5,31 @@ use Ace;
 
 #This script compare GSE_PMID.txt with citace, to get the corresponding table for Paper and GEO accession number.
 
+my $line;
+my @tmp;
+my $tmp_length;
+my %notCuratible;
+
+#--------- Read ecPaperList_merged.csv -------------------
+open (LIST, "/home/wen/LargeDataSets/ExprCluster/ExprClusterTriage/ExprClusterTriage.csv") || die "can't open ecPaperList_merged.csv!";                    
+while ($line=<LIST>) {
+    next unless ($line =~ /^WBPaper/);
+    
+    chomp ($line);
+    @tmp = split /\t/, $line;
+    $tmp_length = @tmp;
+   
+    if ($tmp[1] =~ /Positive but not curatible/){
+     $notCuratible{$tmp[0]} = $tmp[1];
+    }    
+    next unless ($tmp_length == 5);
+    if ($tmp[4] ne "") {
+	$notCuratible{$tmp[0]} = $tmp[4];
+    }
+}
+close (LIST);
+#---------------------------------------------------
+
 print "connecting to citace ...\n";
 my $tace='/usr/local/bin/tace';
 my $acedbpath='/home/citace/citace';
@@ -22,22 +47,19 @@ my ($platform, $t, $gseNumber, $pmid, $paper);
 
 foreach $paper (@paperList) {
 
-       $database = $paper->get('Database', 2);
+        $database = $paper->get('Database', 2);
 	if ($database eq "PMID") {
 	    $pmid=$paper->get('Database', 3);
 	    $PaperID{$pmid} = $paper;
 	    $PMID{$paper} = $pmid;
 	} 
+}
+@paperList = ();
 
-	if ($paper->Microarray_experiment) {
-	    #$MAcurated{$pmid} = 1;
+$query="query find Paper Microarray_experiment = *";
+@paperList=$db->find($query);
+foreach $paper (@paperList) {
 	    $MAcurated{$paper} = 1;
-	    print "$paper $pmid\n";
-	} else {
-	    #$MAcurated{$pmid} = 0;
-	    $MAcurated{$paper} = 0;
-	}
-
 }
 
 open (IN1, "GSE_PMID.txt") || die "can't open $!";
@@ -72,13 +94,16 @@ while (<IN1>) {
 	}
 	if ($MAcurated{$paper}) {
 	    print OUT "Curated\t$paper\t$pmid\t$platform\t$gseNumber\n"; 
-	} else {
-	    print OUT "New\t$paper\t$pmid\t$platform\t$gseNumber\n";
-	    print NEW "$paper\n";
-	    print NEW "\/\/Yes.\n";
-	    print NEW "\/\/pmid$pmid\n";
-	    print NEW "\/\/$gseNumber\n";
-	    print NEW "\/\/$platform\n\n";
+	} elsif ($notCuratible{$paper}) {
+	    print OUT "Not-Curatible\t$paper\t$pmid\t$platform\t$gseNumber\n";
+
+	} else {	    
+		print OUT "New\t$paper\t$pmid\t$platform\t$gseNumber\n";
+		print NEW "$paper\n";
+		print NEW "\/\/Yes.\n";
+		print NEW "\/\/pmid$pmid\n";
+		print NEW "\/\/$gseNumber\n";
+		print NEW "\/\/$platform\n\n";
 	} 
     }
 }
